d5f3eda29ad346103f94465c1840671176d0df4d
Pulak5 <pulak@example.com>
2026-05-28 17:14:50 +0530

    adding application interface fot this project

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -41,8 +41,47 @@ class ModelTrainer:
                 "CatBoosting Regressor":CatBoostRegressor(),
                 "Support Vector Regressor":SVR()
             }
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting Regressor":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "Support Vector Regressor":{},
+                "K-Nearest Neighbours":{},
+                "Xgboost regressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "Adaboosting Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
             model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,
-                                              models=models)
+                                              models=models,param=params)
             
             best_model_name,best_model_score=sorted(model_report.items(),key=lambda x:x[1],reverse=True)[0]
 
@@ -68,4 +107,6 @@ class ModelTrainer:
         return self.initiate_model_trainer(train_array=train_set,test_array=test_set,preprocessor_path=path)
 
 
-    
+if __name__=="__main__":
+    obj=ModelTrainer()
+    print(obj.model_trainer())
