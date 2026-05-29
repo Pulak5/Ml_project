15329917d4f593eb21b3fa5095eb1c17ed5c1175
Pulak5 <147647040+Pulak5@users.noreply.github.com>
2026-05-29 12:23:26 +0530

    Remove CatBoostRegressor from model trainer
    Removed CatBoostRegressor and its parameters from model selection.

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -2,7 +2,6 @@ import os
 import sys
 from dataclasses import dataclass
 
-from catboost import CatBoostRegressor
 from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor,GradientBoostingRegressor
 from sklearn.linear_model import LinearRegression
 from xgboost import XGBRegressor
@@ -38,50 +37,10 @@ class ModelTrainer:
                 "K-Nearest Neighbours":KNeighborsRegressor(),
                 "Adaboosting Regressor":AdaBoostRegressor(),
                 "Gradient Boosting Regressor":GradientBoostingRegressor(),
-                "CatBoosting Regressor":CatBoostRegressor(),
                 "Support Vector Regressor":SVR()
             }
-            params={
-                "Decision Tree": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    # 'splitter':['best','random'],
-                    # 'max_features':['sqrt','log2'],
-                },
-                "Random Forest":{
-                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                 
-                    # 'max_features':['sqrt','log2',None],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "Gradient Boosting Regressor":{
-                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate':[.1,.01,.05,.001],
-                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    # 'criterion':['squared_error', 'friedman_mse'],
-                    # 'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "Linear Regression":{},
-                "Support Vector Regressor":{},
-                "K-Nearest Neighbours":{},
-                "Xgboost regressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "CatBoosting Regressor":{
-                    'depth': [6,8,10],
-                    'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [30, 50, 100]
-                },
-                "Adaboosting Regressor":{
-                    'learning_rate':[.1,.01,0.5,.001],
-                    # 'loss':['linear','square','exponential'],
-                    'n_estimators': [8,16,32,64,128,256]
-                }
-                
-            }
             model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,
-                                              models=models,param=params)
+                                              models=models)
             
             best_model_name,best_model_score=sorted(model_report.items(),key=lambda x:x[1],reverse=True)[0]
 
