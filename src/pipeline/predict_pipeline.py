bd7214f206d07a8418a1de1dbb2f8d8d2bbd8023
Pulak5 <147647040+Pulak5@users.noreply.github.com>
2026-05-29 14:37:59 +0530

    Make model and preprocessor paths dynamic
    Updated model and preprocessor paths to be dynamic based on the script's location.

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
@@ -10,8 +11,9 @@ class Predictionpipeline:
 
     def predict(self,features):
         try:
-            model_path='artifacts\model.pkl'
-            preprocessor_path='artifacts\preprocessor.pkl'
+            BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+            model_path = os.path.join(BASE_DIR, '..', '..', 'artifacts', 'model.pkl')
+            preprocessor_path = os.path.join(BASE_DIR, '..', '..', 'artifacts', 'preprocessor.pkl')
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
 
